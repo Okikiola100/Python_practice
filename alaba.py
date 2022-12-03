3a08ffa5fef51d6c79a4bc7d942d22a3bc38f722
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-03 23:48:21 +0100

    God help me

---
+++ alaba.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+name = "Okikiola"
+last_name = "Oladapo"
+print("hello, {} {}".format(last_name, name))
---
+++ mosh.py
@@ -0,0 +1,21 @@
+#!~/anaconda3/python
+print("Oladapo Okikiola")
+print('o----')
+print(' ||||')
+print('*' * 10)
+price = 10
+print(price)
+rating = 4.9
+name = 'Okikiola'
+is_published = True
+print(price)
+# Taking the information of a man in the hospital
+full_name = "John Smith"
+age = 20
+is_new = True or False
+new = input("Is john smith a new patient(True or False): ")
+if new == True:
+    new == True
+else:
+    new == False
+print("{}, {} years old, new patient:{is_new}".format(full_name, age, is_new = new))
