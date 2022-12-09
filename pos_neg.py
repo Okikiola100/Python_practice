cb23cae5887fdc3fd2a315f8c00b928b9680f12a
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-09 08:27:20 +0100

    Python is sweet

--- pos_neg.py
+++ pos_neg.py
@@ -3,6 +3,7 @@ x = int(input("Please enter a number: "))
 if x < 0:
     print("The number is enter is a negative number")
 elif x == 0:
-    print("The number you entered is a negative number")
-else x > 0:
-    print("
+    print("The number you entered 0(zero)")
+else:
+    print("The number is a positive number")
+print("done")
