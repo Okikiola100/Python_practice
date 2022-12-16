a8d6ccaac0b794b7d794508e7aacef414d0bef1b
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-16 07:20:52 +0100

    Python is good

---
+++ cold_hot.py
@@ -0,0 +1,13 @@
+#!/C/Users/'Azubike Chiefo'/AppData/Local/Programs/Python/Python38-32/python /bin/python
+# Author: Oladapo Okikiola
+temp = int(input("Enter temperature(in celsius): "))
+if temp > 24:
+     print("It's a hot day")
+     print("Drink plenty of water")
+elif temp < 19:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's neither hot nor cold")
+    print("It's a lovely day")
+print("Enjoy our day")
