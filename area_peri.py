8a0d7e7ce1c4be673dcea2c2399b4072153b94d9
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-22 22:24:26 +0100

    Python learning moving forward

---
+++ area_peri.py
@@ -0,0 +1,4 @@
+# Author: Oladapo Okikiola
+print("""
+Author: Oladapo Okikiola\n
+Description: Code that prints the area and perimeter of plane shapes from inputed value""")
--- tictactoe.py
+++ tictactoe.py
@@ -1,49 +1,53 @@
 # Author: Oladapo Okikiola
-from getpass import getpass as input
 print("E P I C     B A T T L E (T I C_T A C_T O E)")
 print()
 print("Select your move")
 print("R - for rock, P - paper, S - scissors")
 
-play_limit = 3
+play_limit = 4
 player1score = 0
 player2score = 0
 playerscore = player1score + player2score
-while playerscore < play_limit:
+while True:
     player1move = input("Player 1> ")
     player2move = input("\nPlayer 2> ")
     print()
-    if player1move == "R":
-        if player2move == "R":
-            print("Draw")
-        elif player2move == "P":
-            print("Player 2 wins")
-            player2score += 1
-        elif player2move == "S":
-            print("Player 1 wins")
-            player1score += 1
+    while playerscore < play_limit:
+        if player1move == "R":
+            if player2move == "R":
+                print("Draw")
+            elif player2move == "P":
+                print("Player 2 wins")
+                player2score += 1
+            elif player2move == "S":
+                print("Player 1 wins")
+                player1score += 1
+            else:
+                print("invalid input by player2")
+        elif player1move == "P":
+            if player2move == "P":
+                print("draw")
+            elif player2move == "R":
+                print("player 1 wins")
+                player1score += 1
+            elif player2move == "S":
+                print("player 2 wins")
+                player2score += 1
+            else:
+                print("Invalid input entered by player2")
+        elif player1move == "S":
+            if player2move == "S":
+                print("draw")
+            elif player2move == "P":
+                print("player 1 wins")
+                player1score += 1
+            elif player2move == "R":
+                print("player 2 wins")
+                player2score += 1
+            else:
+                print("Invalid input entered by player 2")
         else:
-            print("invalid input by player2")
-    elif player1move == "P":
-        if player2move == "P":
-            print("draw")
-        elif player2move == "R":
-            print("player 1 wins")
-            player1score += 1
-        elif player2move == "S":
-            print("player 2 wins")
-            player2score += 1
-        else:
-            print("Invalid input entered by player2")
-    elif player1move == "S":
-        if player2move == "S":
-            print("draw")
-        elif player2move == "P":
-            print("player 1 wins")
-            player1score += 1
-        elif player2move == "R":
-            print("player 2 wins")
-            player2score += 1
-
-print("Player 1 has {} wins.".format(player1score))
-print("Player 2 has {} wins.".format(player2score))
+            print("Invalid input entered by player 1")
+    else:
+        print("\n\nPlayer 1 has {} wins.".format(player1score))
+        print("Player 2 has {} wins.".format(player2score))
