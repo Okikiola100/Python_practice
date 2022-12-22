42c4360a99aed0c81b181ec264f27124849959ac
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-22 22:44:04 +0100

    Python learning moving forward

--- area_peri.py
+++ area_peri.py
@@ -2,3 +2,5 @@
 print("""
 Author: Oladapo Okikiola\n
 Description: Code that prints the area and perimeter of plane shapes from inputed value""")
+plane_shape = ["square", "rectangle", "rhombus", "parrlelogram,"]
+print(plane_shape[:])
