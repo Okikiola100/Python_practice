e5bb3ef43b2d36ab5bcde4beb02d0b69a30cd2ce
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-22 20:27:56 +0100

    remove rm_bg

---
+++ rm_bg.py
@@ -0,0 +1,9 @@
+# Author: Oladapo Okikiola
+pip install rembg
+from rembg import remove
+from PIL import Image
+input_path = 'pic_1.jpg'
+output_path = 'removed.png'
+input = Image.open(input_path)
+output = remove(input)
+output.save(output_path)
