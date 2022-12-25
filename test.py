6c09852c71656dba133231ce21021391195acfb7
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-25 20:06:51 +0100

    nice

--- test.py
+++ test.py
@@ -1,2 +1,12 @@
-taking = "alabva is good"
-print(taking.find('v'), "# he is a very good boy", end=' ')
+def ask_ok(prompt, retries=4, reminder='Please try again!'):
+    while True:
+        ok = input(prompt)
+        if ok in ('y', 'ye', 'yes'):
+            return True
+        if ok in ('n', 'no', 'nop', 'nope'):
+            return False
+        retries = retries - 1
+        if retries < 0:
+            raise ValueError('invalid user response')
+        print(reminder)
+ask_ok('are you ready to learn ')
