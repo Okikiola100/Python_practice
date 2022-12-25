4cd225c57671d0e3fe60657c46bb844869af1892
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-25 23:45:12 +0100

    Done

--- count.py
+++ count.py
@@ -1,12 +1,11 @@
 # Author: Oladapo Okikiola
 def count(n):
-    while True:
-        if n <= -1:
-            print("Count")
-        elif n >= 10:
-            print("Loop have been reached")
-            break
-        else:
-            print(n)
-        count(n --1)
+    if n <= -1:
+        print("Started counting")
+    elif n == 10:
+        print("Loop have been reached")
+        exit()
+    else:
+        print(n)
+    count(n--1)
 count(-1)
