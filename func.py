23fe82d625260a04ed7024c23f5884df53f5bc57
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-25 20:39:15 +0100

    done

---
+++ func.py
@@ -0,0 +1,7 @@
+# Author: Oladapo Okikiola
+def add(a : int, b : int):
+    a = 1
+    b = 2
+    sum = a + b
+    print(f"{a} + {b} = {sum}")
+add(a, b)
---
+++ practice.py
@@ -0,0 +1,4 @@
+# Auhtor: Oladapo Okikiola
+list = ["alaba", "is", "a", "good", "boy"]
+for l in range(len(list)):
+    print(l, list[l], len(l), end="")
