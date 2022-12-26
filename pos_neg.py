13b518d2ba24151edd24c1e9cfbe10b6731bf880
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-26 19:11:03 +0100

    Continue

--- pos_neg.py
+++ pos_neg.py
@@ -1,9 +1,10 @@
 # Author: Oladapo Okikiola
-x = int(input("Please enter a number: "))
-if x < 0:
-    print("The number is enter is a negative number")
-elif x == 0:
-    print("The number you entered 0(zero)")
-else:
-    print("The number is a positive number")
+def random(x):
+    if x < 0:
+        print("The number is enter is a negative number")
+    elif x == 0:
+        print("The number you entered 0(zero)")
+    else:
+        print("The number is a positive number")
+random(x = int(input("Please enter a number: ")))
 print("done")
