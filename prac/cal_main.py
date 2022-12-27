bfbcad2bce26f64696cb11889e95b1263f6a2866
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-27 20:19:50 +0100

    hmm

---
+++ prac/cal_main.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+add = __import__("cal").add
+print(add(1, 2))
---
+++ prac/my_cal.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+def add(a, b):
+
+    """
+    My addition function
+
+    Args:
+    a: integer
+    b: integer
+
+    Returns:
+    addition of a and b, add(a, b)
+    """
+    return (a + b)
+def sub(a, b):
+    
+    """
+    My substraction function
+
+    Args:
+    a: integer
+    b: integer
+
+    Return:
+    Subtraction of b from a, sub(a, b)
+    """
+    return (a - b)
+def mul(a, b):
+
+    """
+    My multiplication function
+
+    Args:
+    a: integer
+    b: integer
+
+    Return:
+    Multiplication of a and b, mul(a, b)
+    """
+    return (a * b)
+def div(a, b):
+
+    """
+    My division function
+
+    Args:
+    a: integer
+    b: integer
+
+    Return:
+    Division of a by b, div(a, b)
+    """
+    return (a / b)
