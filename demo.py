18e496a519cd6e87346b295fea73af127e1ab206
Oladapo <okikiolaoladapo10@gmail.com>
2022-12-28 20:41:43 +0100

    done

---
+++ demo.py
@@ -0,0 +1,3 @@
+import sys
+i = sys.argv
+print(i)
--- path.py
+++ path.py
@@ -2,3 +2,7 @@
 import sys
 
 print('The command line arguments are:')
+for i in sys.argv:
+    print(i)
+
+print('\n\nThe python path is ', sys.path)
